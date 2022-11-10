15669a449ee89a1ce42712f856dbef127875cf37
Đỗ Việt Thắng  <thangdv1@etc.vn>
2022-11-10 17:17:14 +0700

    update1

--- project_1/myproject/myproject/settings.py
+++ project_1/myproject/myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "products.apps.ProductsConfig"
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'mydatabase',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
     }
 }
 
